4a730aa4a9f68cc118a9a85b7e4f635f2ec60f00
hattori ryuki <ryuki.97913795@i.softbank.jp>
2024-01-01 03:20:15 +0900

    fix: Usersテーブルimageカラムにlimitを追記

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_31_181224) do
+ActiveRecord::Schema.define(version: 2023_12_31_181653) do
 
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2023_12_31_181224) do
     t.string "unconfirmed_email"
     t.string "name"
     t.string "nickname"
-    t.text "image"
+    t.text "image", size: :long
     t.string "email"
     t.text "tokens"
     t.datetime "created_at", precision: 6, null: false
