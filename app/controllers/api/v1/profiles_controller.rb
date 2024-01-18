e4d8f75ffaca3087e27ec28fce112ea885435c29
hattori ryuki <ryuki.97913795@i.softbank.jp>
2024-01-18 21:29:36 +0900

    fix: CookieOverflowエラーを修正

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::ProfilesController < ApplicationController
   before_action :set_user, only: %i[show]
 
   def show
+    request.session_options[:skip] = true
     render json: @user.to_json(only: [:id, :name, :image, :email], include: :posts)
   end
 
