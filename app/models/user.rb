7731b5c22e4396490f3116cd0352a2a706784178
hattori ryuki <ryuki.97913795@i.softbank.jp>
2024-02-07 16:02:22 +0900

    feat: userモデルにゲストログインのクラスメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.password_confirmation = user.password
+      user.name = 'ゲストユーザー'
+    end
+  end
 end
