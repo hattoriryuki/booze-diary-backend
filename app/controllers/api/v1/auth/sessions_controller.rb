c91d5b0b8330f30171f5d7e30506ef9ea65eeb6c
hattori ryuki <ryuki.97913795@i.softbank.jp>
2024-02-07 16:06:25 +0900

    feat: sessions_controllerにゲストログインメソッドを追加

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,14 +1,20 @@
-class Api::V1::Auth::SessionsController < ApplicationController
+class Api::V1::Auth::SessionsController < Devise::SessionsController
   def index
     if current_api_v1_user
       render json: { is_login: true, data: current_api_v1_user }
     else
-      render json: { is_login: false, message: "ユーザーが存在しません" }
+      render json: { is_login: false, message: 'ユーザーが存在しません' }
     end
   end
 
   def destroy
     reset_session
     render json: { status: 200, is_logout: true }
+  end
+
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    render json: user
   end
 end
