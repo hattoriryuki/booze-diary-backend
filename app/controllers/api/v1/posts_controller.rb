877ee618a592fa7675e8f7ff4f65aa219423af5b
hattori ryuki <ryuki.97913795@i.softbank.jp>
2024-02-12 17:37:13 +0900

    refactor: 未ログイン時のアクセス制限を追加

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::PostsController < ApplicationController
   before_action :set_post, only: %i[show update destroy]
+  before_action :authenticate_user!, only: %i[create update destroy]
   def index
     posts = Post.all.includes(:user).order(created_at: :desc)
     render json: posts.to_json(include: { user: { only: [:id, :name, :image] } })
--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ProfilesController < ApplicationController
   before_action :set_user, only: %i[show update]
+  before_action :authenticate_user!, only: %i[update]
 
   def show
     render json: @user.to_json(only: [:id, :name, :image, :email], include: :posts)
